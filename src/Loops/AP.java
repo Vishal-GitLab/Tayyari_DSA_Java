5b6871b6bcd411f68af30cf3e8bc1ad7ab26bdf0
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-01-13 21:09:13 +0530

    Add files via upload

---
+++ src/Loops/AP.java
@@ -0,0 +1,22 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class AP {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int  n = input.nextInt();
+
+
+        int a = 2, d = 3;
+        for (int i = 1; i <= n; i++) {
+            System.out.println(a + " ");
+            a += d;
+        }
+//        int n = input.nextInt();
+//        for (int i=2; i<=3*n-1; i+=3) {
+//            System.out.println(i);
+//        }
+    }
+}
