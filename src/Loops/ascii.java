b44e191b0a58079e77ee1428e5b72669834a00b8
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-01-14 19:19:32 +0530

    Add files via upload

---
+++ src/Loops/ascii.java
@@ -0,0 +1,11 @@
+package Loops;
+
+public class ascii {
+    public static void main(String[] args) {
+
+
+        for (int i = 65; i <=90 ; i++) {
+            System.out.println((char)i + " -> " + i);
+        }
+    }
+}
