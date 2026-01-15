f528fd590d8953c2f70bc3edde700523322abd61
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-01-16 00:05:35 +0530

    Add files via upload

---
+++ src/Pattern_Printing/StarRectangle.java
@@ -0,0 +1,19 @@
+package Pattern_Printing;
+
+import java.util.Scanner;
+
+public class StarRectangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int row = input.nextInt();
+        int col = input.nextInt();
+        for (int i = 1; i <= row; i++) {   // kitni lines hogi
+            for (int j = 1; j <= col ; j++) {  // har line me kitna print hoga
+                System.out.print(" * ");
+
+            }
+            System.out.println();
+
+        }
+    }
+}
