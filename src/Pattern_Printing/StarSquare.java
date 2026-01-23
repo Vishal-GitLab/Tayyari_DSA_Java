d7e9549ea4c298583bf406dc0d36a142795d2f06
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-01-23 21:54:00 +0530

    Add files via upload

---
+++ src/Pattern_Printing/StarSquare.java
@@ -0,0 +1,17 @@
+package Pattern_Printing;
+
+import java.util.Scanner;
+
+public class StarSquare {
+    public static void main(String[] args) {
+        Scanner input =  new Scanner(System.in);
+        int n =  input.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
