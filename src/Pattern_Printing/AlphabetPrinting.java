045ad17da37f38752d4984309c25e181062204e7
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-01-23 21:53:10 +0530

    Add files via upload

---
+++ src/Pattern_Printing/AlphabetPrinting.java
@@ -0,0 +1,18 @@
+package Pattern_Printing;
+
+import java.util.Scanner;
+
+public class AlphabetPrinting {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <=n; j++) {
+              //  System.out.print((char)(j+64)+" ");
+                System.out.print((char)(j+96)+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
