2e5a83ac33027dff22075fdec7b2782d40cc6a3b
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-01-25 23:07:37 +0530

    Add files via upload

---
+++ src/Pattern_Printing/NumberSquare.java
@@ -0,0 +1,18 @@
+package Pattern_Printing;
+
+import java.util.Scanner;
+
+public class NumberSquare {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int  n = input.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n ; j++) {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
