784f0f3154fd546f6cd12a7c3b9246dcf5f5d9e1
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-01-25 23:08:49 +0530

    Add files via upload

---
+++ src/Pattern_Printing/OddTriangle.java
@@ -0,0 +1,28 @@
+package Pattern_Printing;
+
+import java.util.Scanner;
+
+public class OddTriangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <=i; j++) {
+                System.out.print( 2*j-1  + " ");
+
+            }
+            System.out.println();
+             }
+
+
+//        for (int i = 1; i <= n; i++) {
+//            int a  = 1;
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print(a + " ");
+//                a += 2;
+//            }
+//            System.out.println();
+//        }
+    }
+}
