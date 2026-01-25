21c56f44df573b7078076a18ad33eef7ca693403
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-01-25 23:06:11 +0530

    Add files via upload

---
+++ src/Pattern_Printing/FloydTriangle.java
@@ -0,0 +1,20 @@
+package Pattern_Printing;
+
+import java.util.Scanner;
+
+public class FloydTriangle {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        int  a  = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(a++  +" ");
+
+            }
+            System.out.println();
+        }
+    }
+}
