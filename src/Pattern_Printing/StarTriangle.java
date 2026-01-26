13f1df502f884cd69d7bdc3a1e9d912417c975d6
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-01-26 22:16:19 +0530

    Add files via upload

---
+++ src/Pattern_Printing/StarTriangle.java
@@ -0,0 +1,19 @@
+package Pattern_Printing;
+
+import java.util.Scanner;
+
+public class StarTriangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <=i; j++) {
+                System.out.print("*");
+            //    System.out.print(j);                     // for number triangle
+
+            }
+            System.out.println();
+        }
+    }
+}
