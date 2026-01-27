ae6f131adc888ba1a689d257c7e7d11f3781d854
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-01-27 23:36:44 +0530

    Add files via upload

---
+++ src/Function/BuiltIn.java
@@ -0,0 +1,14 @@
+package Function;
+
+public class BuiltIn {
+    public static void main(String[] args) {
+//        System.out.println(Math.sqrt(100));
+//        System.out.println(Math.cbrt(100));
+
+        System.out.println(Math.ceil(3.0001));
+
+
+        System.out.println(Math.min(3.78, 9 ));
+        System.out.println(Math.max(2.4, 2.41));
+    }
+}
