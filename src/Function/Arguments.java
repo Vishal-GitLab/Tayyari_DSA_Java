a1bcfb0c13d7f0bf0247817ad3231ced347413df
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-01-29 23:33:00 +0530

    Add files via upload

---
+++ src/Function/Arguments.java
@@ -0,0 +1,13 @@
+package Function;
+
+public class Arguments {
+    public static void max(int a, int b, int c) {
+         if (a>=b&& a>=c) System.out.println(a);
+         else if (b>=a && b>=c) System.out.println(b);
+         else System.out.println(c);
+
+    }
+    public static void main(String[] args) {
+        max(5,3,2);
+    }
+}
