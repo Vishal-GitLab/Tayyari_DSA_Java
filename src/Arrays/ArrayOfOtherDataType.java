c27b4200130cb9491fa48cf34f5b048aa97f03d7
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-02-04 22:44:04 +0530

    Add files via upload

---
+++ src/Arrays/ArrayOfOtherDataType.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+public class ArrayOfOtherDataType {
+    public static void main(String[] args) {
+//        char[] arr = new char[8];
+//        for (int i = 0; i < arr.length; i++) {
+//            System.out.print(arr[i]+" ");
+//        }
+
+        String[] arr = {"Abhinav","Vishal","Alok","Arun"};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+}
