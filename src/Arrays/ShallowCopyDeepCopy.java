1c8508479a181a6bd925049fec03e138d55d4e83
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-02-06 22:02:49 +0530

    Add files via upload

---
+++ src/Arrays/ShallowCopyDeepCopy.java
@@ -0,0 +1,16 @@
+package Arrays;
+import java.util.Arrays;
+public class ShallowCopyDeepCopy {
+    public static void main(String[] args) {
+        int a  = 4;        // 4 bytes leta hai
+        int[] arr = {10,20,30,40};    // 16 byte
+//        int[] x =  arr;            // x is shallow copy of arr
+//        x[0] = 100;
+//
+
+        int[] y = Arrays.copyOf(arr, arr.length);
+        y[0] = 100;
+        System.out.println(y[0]);
+        System.out.println(arr[0]);
+    }
+}
