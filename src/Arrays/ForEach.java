3b1f7d43a0c929cc7fc7d02c5afeb4528f50ea52
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-02-09 22:37:28 +0530

    Add files via upload

---
+++ src/Arrays/ForEach.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class ForEach {
+    public static void main(String[] args) {
+        int[] arr = {2,4,5,8,10};
+
+        for (int ele : arr) {
+            System.out.print(ele + " ");
+        }
+    }
+}
