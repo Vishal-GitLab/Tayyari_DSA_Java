4fc74cb232ab054b29f0b87005898cfeb4468888
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-02-10 22:30:34 +0530

    Add files via upload

---
+++ src/Arrays/RotateArray.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class RotateArray {
+    public static void main(String[] args) {
+        int[] arr = {6,8,1,2,4,9,0};
+        int d = 4;
+        int n = arr.length;
+        d  = d%n;
+        reverse(arr,0,d-1);
+        reverse(arr,d,n-1);
+        reverse(arr,0,n-1);
+        for (int ele:arr) System.out.print(ele+ " ");
+
+    }
+    static void reverse(int[] arr, int i, int j) {
+        while(i<j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
