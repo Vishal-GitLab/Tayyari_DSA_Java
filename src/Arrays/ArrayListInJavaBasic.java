0392a9887237d4413e55952e2abdbb19232a1a4e
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-02-13 22:15:56 +0530

    Update ArrayListInJavaBasic.java

--- src/Arrays/ArrayListInJavaBasic.java
+++ src/Arrays/ArrayListInJavaBasic.java
@@ -1,6 +1,8 @@
 package Arrays;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class ArrayListInJavaBasic {
     public static void main(String[] args) {
@@ -13,7 +15,22 @@ public class ArrayListInJavaBasic {
         System.out.println(arr.get(2));      // arr[2]
         arr.set(3,50);         // for update arr index
 
-        System.out.println(arr);
+        System.out.println(arr);              // not traversing array by ourselves
+        int n = arr.size();     // array list ki length nhi hoti hai size hoti hai
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr.get(i)+ " ");
+        }
 
+        System.out.println();
+        for (int ele : arr) {
+            System.out.print(ele +  " ");
+        }
+        arr.add(78);
+        arr.add(1,100);
+        System.out.println(arr);
+        arr.remove(arr.size()-1);
+        System.out.println(arr);
+        Collections.reverse(arr);
+        System.out.println(arr);
     }
 }
