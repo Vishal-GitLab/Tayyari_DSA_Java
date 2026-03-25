4bcf8f60fdf8f1baa0f28e5f793d7709494f1d5b
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-03-25 23:33:18 +0530

    Add files via upload

---
+++ src/Recursion/PrintSum.java
@@ -0,0 +1,18 @@
+package Recursion;
+
+public class PrintSum {
+    public static void main(String[] args) {
+        String s  = "abc";
+        subset("",s,0);
+    }
+
+    private static void subset(String ans, String s, int idx) {
+        if (idx==s.length()) {
+            System.out.print(ans+ " ");
+            return;
+        }
+        char ch = s.charAt(idx);
+        subset(ans+ch,s,idx+1);   // pick
+        subset(ans,s,idx+1); // skip
+    }
+}
